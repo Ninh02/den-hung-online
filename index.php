73f65bbe78b830b0fba5d860de0b8e6b345d68a3
Your Name <you@example.com>
2021-06-19 15:57:47 +0700

    Layout

--- index.php
+++ index.php
@@ -1,1 +1,304 @@
 <?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Freelancer - Start Bootstrap Theme</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="public/assets/favicon.ico" />
+    <!-- Font Awesome icons (free version)-->
+    <script src="https://use.fontawesome.com/releases/v5.15.3/js/all.js" crossorigin="anonymous"></script>
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="public/css/styles.css" rel="stylesheet" />
+</head>
+<body id="page-top">
+<!-- Navigation-->
+<nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
+    <div class="container">
+        <a class="navbar-brand" href="#page-top">Đền Hùng online</a>
+        <button class="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            Menu
+            <i class="fas fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#portfolio">Lộ trình</a></li>
+                <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#about">Giới thiệu</a></li>
+                <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#contact">Liên hệ</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<!-- Masthead-->
+<header class="masthead bg-primary text-white text-center">
+    <div class="container d-flex align-items-center flex-column">
+        <!-- Masthead Heading-->
+        <h1 class="masthead-heading text-uppercase mb-0">Đền Hùng</h1>
+        <!-- Icon Divider-->
+        <div class="divider-custom divider-light">
+            <div class="divider-custom-line"></div>
+            <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+            <div class="divider-custom-line"></div>
+        </div>
+        <!-- Masthead Subheading-->
+        <p class="masthead-subheading font-weight-light mb-0">Dù ai đi ngược về xuôi</p>
+        <p class="masthead-subheading font-weight-light mb-0">Nhớ ngày Giỗ Tổ mồng mười tháng ba</p>
+    </div>
+</header>
+<!-- Portfolio Section-->
+<section class="page-section portfolio" id="portfolio">
+    <div class="container">
+        <!-- Portfolio Section Heading-->
+        <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Lộ trình</h2>
+        <!-- Icon Divider-->
+        <div class="divider-custom">
+            <div class="divider-custom-line"></div>
+            <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+            <div class="divider-custom-line"></div>
+        </div>
+        <!-- Portfolio Grid Items-->
+        <div class="row justify-content-center">
+            <!-- Portfolio Item 1-->
+            <div class="col-md-6 col-lg-6 mb-5 text-center">
+                <div class="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal1">
+                    <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                        <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
+                    </div>
+                    <img class="img-fluid" src="public/image/den_ha.png" alt="..." />
+                    <p>Đền hạ</p>
+                </div>
+            </div>
+            <!-- Portfolio Item 2-->
+            <div class="col-md-6 col-lg-6 mb-5 text-center">
+                <div class="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal2">
+                    <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                        <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
+                    </div>
+                    <img class="img-fluid" src="public/image/den_trung.png" alt="..." />
+                    <p>Đền trung</p>
+                </div>
+            </div>
+            <!-- Portfolio Item 3-->
+            <div class="col-md-6 col-lg-6 mb-5 text-center">
+                <div class="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal3">
+                    <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                        <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
+                    </div>
+                    <img class="img-fluid" src="public/image/dlen_thuong.png" alt="..." />
+                    <p>Đền thượng</p>
+                </div>
+            </div>
+            <!-- Portfolio Item 4-->
+            <div class="col-md-6 col-lg-6 mb-5 text-center">
+                <div class="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal4">
+                    <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                        <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
+                    </div>
+                    <img class="img-fluid" src="public/image/den_gieng_2.jpg" alt="..." />
+                    <p>Đền giếng</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Footer-->
+<footer class="footer text-center">
+    <div class="container">
+        <div class="row">
+            <!-- Footer Location-->
+            <div class="col-lg-6 mb-5 mb-lg-0">
+                <h4 class="text-uppercase mb-4">Địa chỉ</h4>
+                <p class="lead mb-0">
+                    Thôn Cổ Tích - Xã Hy Cương - Thành phố Việt Trì - Tỉnh Phú Thọ
+                </p>
+            </div>
+            <!-- Footer About Text-->
+            <div class="col-lg-6">
+                <h4 class="text-uppercase mb-4">Thông tin liên hệ</h4>
+                <p class="lead mb-0">
+                    Ban Quản Lý Khu Di Tích Đền Hùng
+                    <br>
+                    Địa chỉ: Hy Cương, Thành phố Việt Trì, Phú Thọ
+                    <br>
+                    Giám đốc: Nguyễn Xuân Các
+                    <br>
+                    Điện thoại: 0210.3860026
+                    <br>
+                    Fax: 0210.3860752
+                </p>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- Copyright Section-->
+<div class="copyright py-4 text-center text-white">
+    <div class="container"><small>Copyright &copy; Your Website 2021</small></div>
+</div>
+<!-- Portfolio Modals-->
+<!-- Portfolio Modal 1-->
+<div class="portfolio-modal modal fade" id="portfolioModal1" tabindex="-1" aria-labelledby="portfolioModal1" aria-hidden="true">
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+            <div class="modal-header border-0"><button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button></div>
+            <div class="modal-body text-center pb-5">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-8">
+                            <!-- Portfolio Modal - Title-->
+                            <h2 class="portfolio-modal-title text-secondary text-uppercase mb-0">Đền hạ</h2>
+                            <!-- Icon Divider-->
+                            <div class="divider-custom">
+                                <div class="divider-custom-line"></div>
+                                <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                                <div class="divider-custom-line"></div>
+                            </div>
+                            <!-- Portfolio Modal - Image-->
+                            <img class="img-fluid rounded mb-5" src="public/image/den_ha.png" alt="..." />
+                            <!-- Portfolio Modal - Text-->
+                            <p class="mb-4">Tương truyền rằng nơi đây, mẹ Âu Cơ sinh ra bọc trăm trứng, sau nở thành trăm người con trai. Khi các con khôn lớn, cha Lạc Long Quân mang theo 50 người con về vùng biển quai đê lấn biển, mở mang bờ cõi. Mẹ Âu Cơ mang theo 49 người con ngược lên vùng núi, trồng dâu, chăn tằm, dệt vải, xây dựng cuộc sống. Người con trưởng ở lại làm Vua, dựng nên Nhà nước Văn Lang, truyền được 18 đời đều gọi Hùng Vương.
+
+                                Đền Hạ được xây dựng vào thời Hậu Lê (thế kỷ XVII - XVIII). Vào thời nhà Nguyễn, thế kỷ XX (năm 1997), Đền được trùng tu tôn tạo. Năm 2010, UBND thành phố Hà Nội công đức tiền tu bổ tôn tạo như hiện nay.
+
+                                Đền Hạ thờ Thần Núi, 18 đời Vua Hùng và 2 bà công chúa Tiên Dung, Ngọc Hoa - con gái Vua Hùng thứ 18.</p>
+                            <button class="btn btn-primary" href="#!" data-bs-dismiss="modal">
+                                <i class="fas fa-times fa-fw"></i>
+                                Thoát
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Portfolio Modal 2-->
+<div class="portfolio-modal modal fade" id="portfolioModal2" tabindex="-1" aria-labelledby="portfolioModal2" aria-hidden="true">
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+            <div class="modal-header border-0"><button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button></div>
+            <div class="modal-body text-center pb-5">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-8">
+                            <!-- Portfolio Modal - Title-->
+                            <h2 class="portfolio-modal-title text-secondary text-uppercase mb-0">Đền trung</h2>
+                            <!-- Icon Divider-->
+                            <div class="divider-custom">
+                                <div class="divider-custom-line"></div>
+                                <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                                <div class="divider-custom-line"></div>
+                            </div>
+                            <!-- Portfolio Modal - Image-->
+                            <img class="img-fluid rounded mb-5" src="public/image/den_trung.png" alt="..." />
+                            <!-- Portfolio Modal - Text-->
+                            <p class="mb-4">Có tên chữ là Hùng Vương Tổ Miếu. Đền được xây dựng vào thời Trần (thế kỷ XIII). Dưới thời Nguyễn (thế kỷ XIX), Đền Trung được xây dựng lại, kiến trúc có ba gian, quay theo hướng Nam. Tháng 9/2009, Đền Trung được tu bổ, tôn tạo; kiến trúc đền kiểu chữ nhị (=), gồm tiền tế và hậu cung.
+
+                                Đền Trung thờ thần Núi, thờ 18 đời Vua Hùng và 2 bà công chúa Tiên Dung, Ngọc Hoa - con gái Vua Hùng thứ 18.
+
+                                Tương truyền, nơi đây Vua Hùng cùng các Lạc Hầu, Lạc Tướng du ngoạn ngắm cảnh và họp bàn việc nước. Vào đời Hùng Vương thứ 6, đã mở cuộc thi để chọn người hiền, tài kế nghiệp.
+
+                                Lang Liêu là người con trai út đã nghĩ ra cách làm bánh chưng vuông (tượng trưng cho đất), bánh giầy tròn (tượng trưng cho trời), ở giữa bánh chưng có nhân hành, thịt mỡ, đỗ xanh tượng trưng cho vạn vật sinh tồn, bên ngoài gói bằng lá dong xanh tượng trưng cho tình yêu thương đùm bọc của cộng đồng. Vua Hùng khen ngợi bánh thì ngon mà ý thì hay nên đã truyền ngôi cho Lang Liêu nối nghiệp trở thành Hùng Vương thứ 7 (tên húy là Hùng Chiêu Vương).</p>
+                            <button class="btn btn-primary" href="#!" data-bs-dismiss="modal">
+                                <i class="fas fa-times fa-fw"></i>
+                                Thoát
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Portfolio Modal 3-->
+<div class="portfolio-modal modal fade" id="portfolioModal3" tabindex="-1" aria-labelledby="portfolioModal3" aria-hidden="true">
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+            <div class="modal-header border-0"><button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button></div>
+            <div class="modal-body text-center pb-5">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-8">
+                            <!-- Portfolio Modal - Title-->
+                            <h2 class="portfolio-modal-title text-secondary text-uppercase mb-0">Đền thượng</h2>
+                            <!-- Icon Divider-->
+                            <div class="divider-custom">
+                                <div class="divider-custom-line"></div>
+                                <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                                <div class="divider-custom-line"></div>
+                            </div>
+                            <!-- Portfolio Modal - Image-->
+                            <img class="img-fluid rounded mb-5" src="public/image/dlen_thuong.png" alt="..." />
+                            <!-- Portfolio Modal - Text-->
+                            <p class="mb-4">Đền Thượng có tên chữ là "Kính Thiên Lĩnh Điện" (nghĩa là: Điện thờ trời trên núi Nghĩa Lĩnh). Tương truyền: Đây là nơi Vua Hùng và các Lạc Hầu, Lạc Tướng làm lễ cúng tế trời - đất để cầu mong mưa thuận gió hòa, mùa màng tốt tươi, nhân khang vật thịnh.
+
+                                Thế kỷ XV, đền được xây dựng để thờ Thần núi, thờ 18 đời Vua Hùng và hai bà công chúa Tiên Dung, Ngọc Hoa. Trải qua thời gian, Đền Thượng được nhiều lần trùng tu, tôn tạo dưới thời Lê, thời Nguyễn. Năm 2007, đền được đại trùng tu, tôn tạo và mở rộng, xây dựng thêm một số công trình phụ trợ, khu sân vườn cảnh quan để tạo không gian thoáng đãng và rộng rãi cho đồng bào về chiêm bái và tri ân công đức Tiên tổ.
+
+                                Hằng năm, vào ngày Giỗ Tổ Hùng Vương mùng 10 tháng 3 âm lịch, các đồng chí lãnh đạo Đảng, Nhà nước, các Bộ, ngành Trung ương cùng đoàn đại biểu thay mặt cho đồng bào cả nước trang trọng và thành kính tổ chức nghi lễ dâng hương tri ân, công đức Tổ tiên tại Đền Thượng và cầu mong anh linh tổ tiên phù hộ cho quốc thái, dân an, bách gia trăm họ vạn đại trường tồn.</p>
+                            <button class="btn btn-primary" href="#!" data-bs-dismiss="modal">
+                                <i class="fas fa-times fa-fw"></i>
+                                Thoát
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Portfolio Modal 4-->
+<div class="portfolio-modal modal fade" id="portfolioModal4" tabindex="-1" aria-labelledby="portfolioModal4" aria-hidden="true">
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+            <div class="modal-header border-0"><button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button></div>
+            <div class="modal-body text-center pb-5">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-8">
+                            <!-- Portfolio Modal - Title-->
+                            <h2 class="portfolio-modal-title text-secondary text-uppercase mb-0">Đền giếng</h2>
+                            <!-- Icon Divider-->
+                            <div class="divider-custom">
+                                <div class="divider-custom-line"></div>
+                                <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                                <div class="divider-custom-line"></div>
+                            </div>
+                            <!-- Portfolio Modal - Image-->
+                            <img class="img-fluid rounded mb-5" src="public/image/den_gieng_2.jpg" alt="..." />
+                            <!-- Portfolio Modal - Text-->
+                            <p class="mb-4">Tương truyền đây là nơi hai bà công chúa Tiên Dung và Ngọc Hoa - con gái Vua Hùng Vương thứ 18 thường đến đây soi gương chải tóc ở Giếng Ngọc khi theo cha đi kinh lý qua vùng này. Để tưởng nhớ công lao hai bà đã giúp dân trị thủy, khai hoang, trồng lúa nước, nhân dân đã xây dựng đền để phụng thờ muôn đời.
+
+                                Đền được xây dựng vào thời Nguyễn (thế kỷ XIX), kiến trúc kiểu chữ Đinh, quay theo hướng Đông - Nam, gồm 2 tòa: Tiền tế và Hậu cung. Trong hậu cung của đền, trước ban thờ hai bà công chúa vẫn còn chiếc giếng ngọc, nước trong mát quanh năm. Năm 2010, đền được đại trùng tu, tôn tạo và mở rộng thêm kiến trúc như hiện nay.
+
+                                Tại khu vực Đền Giếng, ngày 19/9/1954, Chủ tịch Hồ Chí Minh về thăm viếng Đền Hùng, thắp hương viếng Tổ tiên và chọn Đền Giếng làm nơi gặp mặt và nói chuyện với cán bộ Sư đoàn 308 - Đại đoàn quân Tiên Phong để giao nhiệm vụ tiếp quản Thủ đô Hà Nội, Bác đã căn dặn: “Các vua Hùng đã có công dựng nước/ Bác cháu ta phải cùng nhau giữ lấy nước”.
+
+                                Trải qua thời gian, đền được nhiều lần trùng tu, tôn tạo vào các năm: 1922, 1925, 1999. Năm 2010 đền được đại trùng tu, tôn tạo và mở rộng thêm kiến trúc như hiện nay.</p>
+                            <button class="btn btn-primary" href="#!" data-bs-dismiss="modal">
+                                <i class="fas fa-times fa-fw"></i>
+                                Thoát
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Bootstrap core JS-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"></script>
+<!-- Core theme JS-->
+<script src="public/js/scripts.js"></script>
+<!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+<!-- * *                               SB Forms JS                               * *-->
+<!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+<!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+<script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+</body>
+</html>
+
